2a948d669cecfbe19bc94479b04a69b170408a11
Yasin <yasinpeker43@gmail.com>
2025-02-19 07:29:59 +0300

    setCountry() is changed to renderCountry()

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-document.querySelector("#btnSearch").addEventListener("click",()=> {
-    text=document.querySelector("#txtSearch").values;
-    getCountry(text);
+document.querySelector("#btnSearch").addEventListener("click", ()=> {
+  let text=document.querySelector("#txtSearch").value;
+  getCountry(text);
+});
 
-})
 
 function getCountry(country)
  {
@@ -17,7 +17,7 @@ request.addEventListener("load",function() {
     const data=JSON.parse(this.responseText);
     console.log(typeof data);
     console.log(data);
-    setCountry(data);
+    renderCountry(data[0]);
 
     const countries=data[0].borders.toString();
 
@@ -36,33 +36,46 @@ request.addEventListener("load",function() {
 
 
 function renderCountry(data) {
-    for(let country of data) {
 
-  
-    const html=`
-       <div class="col-3"> 
-          <div class="card h-100">
-          <img src="${country.flags.png}" class="card-img-top">
-          
-           <div class="card-body">
-             <h5 class="card-title">${country.name.common}</h5>
-           </div>
-           <ul class="list-group list-group-flush"> 
-            <li class="list-group-item">Population: ${(country.population/1000000).toFixed(1)}</li>
-            <li class="list-group-item">Capital:${country.capital[0]}</li>
-            <li class="list-group-item">Language:${Object.values(country.languages)}</li>
-           
-           </ul>
-           </div>
-    
-       </div> 
-    `;
-     
-    document.querySelector(".container .row").insertAdjacentHTML("beforeend",html);
-}
+     let html=`
+     <div class="card-header">
+          Search Result
+       </div>
+       <div class="card-body">
+              <div class="row">
+                <div class="col-4">
+                  <img src="${data.flags.png}" alt="" class="img-fluid">
+
+                </div>
+                <div class="col-8">
+                    <div class="card-title">
+                     ${data.name.common}
+                    </div>
+                    <hr>
+                    <div class="row">
+                      <div class="col-4">Population:</div>
+                      <div class="col-8">${(data.population/1000000).toFixed(1)} Million</div>
+                    </div>
+                    <div class="row">
+                      <div class="col-4">Language(s): </div>
+                      <div class="col-8">${Object.values(data.languages)}</div>
+                    </div>
+                    <div class="row">
+                      <div class="col-4">Capital: </div>
+                      <div class="col-8">${data.capital[0]}</div>
+                    </div>
+
+                </div>
+              </div>
+              
+       </div>
+     `;
+
+     document.querySelector("#country-details").innerHTML=html;
 }
 
-displayCountry("türkiye");
+
+
 
 
 
